dac39a47fbfdfcc42482bf6c8b03f9781d988ecb
anadantasp <caroldantasp@gmail.com>
2024-01-29 19:32:43 -0300

    Implementando relacionamentos OneToMany e ManyToOne

--- src/main/java/com/ead/course/models/CourseModel.java
+++ src/main/java/com/ead/course/models/CourseModel.java
@@ -4,11 +4,13 @@ import com.ead.course.enums.CourseLevel;
 import com.ead.course.enums.CourseStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import javax.persistence.*;
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.Set;
 import java.util.UUID;
 
 @Data
@@ -21,32 +23,28 @@ public class CourseModel implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID courseId;
-
     @Column(nullable = false, length = 150)
     private String name;
-
     @Column(nullable = false, length = 250)
     private String description;
-
     @Column
     private String imgUrl;
-
     @Column(nullable = false)
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
     private LocalDateTime creationDate;
-
     @Column(nullable = false)
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
     private LocalDateTime lastUpdateDate;
-
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
     private CourseStatus courseStatus;
-
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
     private CourseLevel courseLevel;
-
     @Column(nullable = false)
     private UUID userInstructor;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @OneToMany(mappedBy = "course")
+    private Set<ModuleModel> modules;
 }
