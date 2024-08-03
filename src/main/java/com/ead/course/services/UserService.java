9961e0d454d7fbd736755a1d4c4eaccf57e56772
anadantasp <caroldantasp@gmail.com>
2024-08-03 18:20:28 -0300

    Criar consumer e receber Event State Transfer

--- src/main/java/com/ead/course/services/UserService.java
+++ src/main/java/com/ead/course/services/UserService.java
@@ -7,4 +7,5 @@ import org.springframework.data.jpa.domain.Specification;
 
 public interface UserService {
     Page<UserModel> findAll(Specification<UserModel> spec, Pageable pageable);
+    UserModel save(UserModel userModel);
 }
